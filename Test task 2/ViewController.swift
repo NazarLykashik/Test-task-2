73c712188860854162a22ca961e8d637cf197daa
Nazar Lukashik <96954224+NazarLykashik@users.noreply.github.com>
2022-12-28 15:50:50 +0300

    Initial Commit

---
+++ Test task 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test task 2
+//
+//  Created by Nazar Lykashik on 28.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
